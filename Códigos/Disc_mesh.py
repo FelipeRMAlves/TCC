7ca616d031b3d80d4ad59495c5a715b05f061429
FelipeRMAlves <felipe.alves@technipfmc.com>
2021-03-24 19:12:40 -0300

    Codigo que gera a geometria e malha do disco

--- Códigos/Disc_mesh.py
+++ Códigos/Disc_mesh.py
@@ -18,36 +18,54 @@ import numpy as np
 # 1) Input - Definicoes da simulacao
 ##############################################################################
 '''
-re = 0.015        # raio externo [m]
-ri = 0.005        # raio interno [m]
-e = 0.005          # espessura [m]
+re = 0.030            # raio externo [m]
+rt = 0.022            # raio interno do trilho [m]
+ri = 0.018            # raio interno [m]
+e = 0.001             # espessura [m]
+le_min = 0.75*e          # Tamanho mínimo dos elementos [m]
+le_max = 0.75*e          # Tamanho máximo dos elementos[m]
 
 
 ##############################################################################
 # Codigo
 ##############################################################################
 gmsh.initialize()
-gmsh.model.add("DFG 3D")
+gmsh.model.add("minha_malha")
 
-# addCylinder(x,y,z)
-cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, e, 0, re, tag = 1)
-cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, e, 0, ri, tag = 2)
-gmsh.model.occ.cut([(3, 3)], [(3, 7)], 8)
+# addCylinder(x,y,z, dx, dy, dz)
+cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, re, tag = 1)
+cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, rt, tag = 2)
+gmsh.model.occ.cut([(3, 1)], [(3, 2)], 3)
+
+
+
+# construcao da superficie do trilho da pastilha
+cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, rt, tag = 4)
+cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, ri, tag = 5)
+gmsh.model.occ.cut([(3, 4)], [(3, 5)], 6)
+
+
+
 
 gmsh.model.occ.synchronize()
 volumes = gmsh.model.getEntities(dim=3)
 
 
 
-
-
 # sincronizar o modelo
 gmsh.model.geo.synchronize()
+
+
+# Definir os tamanhos dos elementos da malha
+gmsh.option.setNumber("Mesh.CharacteristicLengthMin", le_min)
+gmsh.option.setNumber("Mesh.CharacteristicLengthMax", le_max)
+
 
 # Gerar malha
 gmsh.model.mesh.generate(3)  # 3D
 
+
 # Salvar arquivo .msh
-gmsh.write("mesh3D.msh")
+gmsh.write("disc.msh")
 # gmsh.option.setNumber("Mesh.SaveAll", 1)
 gmsh.finalize()
