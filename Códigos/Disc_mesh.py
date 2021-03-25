eda492287f7c51b5513cca7311bbe82144fe2ac6
FelipeRMAlves <felipe.alves@technipfmc.com>
2021-03-24 21:15:29 -0300

    update codigos

--- Códigos/Disc_mesh.py
+++ Códigos/Disc_mesh.py
@@ -7,34 +7,31 @@ class disc():
         import numpy as np
         import gmsh
 
-        # '''
-        # ##############################################################################
-        # # 1) Input - Definicoes da simulacao
-        # ##############################################################################
-        # '''
-        # re = 0.030            # raio externo [m]
-        # rt = 0.022            # raio interno do trilho [m]
-        # ri = 0.018            # raio interno [m]
-        # e = 0.001             # espessura [m]
-        # le_min = 0.75*e          # Tamanho mínimo dos elementos [m]
-        # le_max = 0.75*e          # Tamanho máximo dos elementos[m]
-
-        # in order to output messages on the terminal, just set the 
-        # "General.Terminal" option to 1:
-        gmsh.option.setNumber("General.Terminal", 1)
-
 
         # Before using any functions in the Python API, Gmsh must be initialized:
         gmsh.initialize()
         gmsh.model.add("minha_malha")
 
+
+
+        includeBoundary = True
+
+
+        
+
+        # in order to output messages on the terminal, just set the 
+        # "General.Terminal" option to 1:
+        gmsh.option.setNumber("General.Terminal", 1)
+
         # Adicionar cilindros para contruir o disco
         # addCylinder(x,y,z, dx, dy, dz)
 
         # construcao da superficie do trilho da pastilha
-        cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, re, tag = 1)  # disco externo
+        cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, re, tag = 1)  # exterior trilho
         cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, rt, tag = 2)  # interior trilho
         gmsh.model.occ.cut([(3, 1)], [(3, 2)], 3)  # disco apenas com o trilho
+        # gmsh.model.setPhysicalName(3, 1, "The volume")
+
 
         # construcao do interior do disco alem do trilho
         cylinder = gmsh.model.occ.addCylinder(0, 0, 0, 0, 0, e, rt, tag = 4)
